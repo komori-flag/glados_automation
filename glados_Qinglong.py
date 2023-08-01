83fd70b347b20d97571f4bc910d406dd07273584
lukes <70319988+lukesyy@users.noreply.github.com>
2023-08-01 10:29:30 +0800

    Update glados_Qinglong.py

--- glados_Qinglong.py
+++ glados_Qinglong.py
@@ -1,10 +1,10 @@
 import requests,json,os
 
 # pushplus秘钥
-sckey = os.environ.get("PUSHPLUS_TOKEN", "")
+sckey = os.environ。get("PUSHPLUS_TOKEN"， "")
 sendContent = ''
 # glados账号cookie
-cookies= os.environ.get("GLADOS_COOKIE", []).split("&")
+cookies= os.environ。get("GLADOS_COOKIE"， [])。split("&")
 if cookies[0] == "":
     print('未获取到COOKIE变量') 
     cookies = []
@@ -18,16 +18,16 @@ def start():
     origin = "https://glados.rocks"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
     payload={
-        'token': 'glados.network'
+        'token': 'glados.one'
     }
-    for cookie in cookies:
+    for cookie 在 cookies:
         checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
         state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
     #--------------------------------------------------------------------------------------------------------#  
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         email = state.json()['data']['email']
-        if 'message' in checkin.text:
+        if 'message' 在 checkin.text:
             mess = checkin.json()['message']
             print(email+'----'+mess+'----剩余('+time+')天')  # 日志输出
             global sendContent
