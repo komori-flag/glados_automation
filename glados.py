f014df0ac74d14b41a5371a8d089162b8aaac009
CN-YoungYang <513418740@qq.com>
2024-07-12 20:54:36 +0800

    解决time报错问题
    str(time)格式化后在进行字符切割

--- glados.py
+++ glados.py
@@ -26,13 +26,13 @@ if __name__ == '__main__':
         state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
     #--------------------------------------------------------------------------------------------------------#  
         time = state.json()['data']['leftDays']
-        # time = time.split('.')[0]
+        time = str(time).split('.')[0]
         email = state.json()['data']['email']
         if 'message' in checkin.text:
             mess = checkin.json()['message']
             print("签到成功") # 日志输出
             # print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
-            sendContent += email+'----'+mess+'----剩余('+str(time)+')天\n'
+            sendContent += email+'----'+mess+'----剩余('+time+')天\n'
         else:
             requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
             print('cookie已失效')  # 日志输出
